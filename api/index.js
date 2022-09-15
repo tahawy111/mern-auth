c0dddfbb0d212f3fd70a4f48d9457d95ca561d40
tahawy111 <amer.vib@gmail.com>
2022-09-15 18:16:37 +0200

    .

--- api/index.js
+++ api/index.js
@@ -1,6 +1,28 @@
+const cookieSession = require("cookie-session");
 const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
 const app = express();
 
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["amer"],
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
 app.listen(5000, () => {
-  console.log("Server is running");
+  console.log("Server is running!");
 });
